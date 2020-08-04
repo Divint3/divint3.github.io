9f31d54e8d698849e5e213a56a464681b8048e9b
divint3 <divint3@gmail.com>
2020-08-04 21:20:08 +0800

    Site updated: 2020-08-04 21:20:06

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -33,8 +33,8 @@ set -e
 
 # Default settings
 ZSH=${ZSH:-~/.oh-my-zsh}
-REPO=${REPO:-robbyrussell/oh-my-zsh}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+REPO=${REPO:-zxc0ok/ohmyzsh}
+REMOTE=${REMOTE:-https://gitee.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
 # Other options
